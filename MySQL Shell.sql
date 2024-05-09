16d7d85977e2a9055692a5d3badf68f9097b402d
SQLSurgery <sqlsurgery@gmail.com>
2024-05-09 16:53:45 +0100

    MySQL

---
+++ MySQL Shell.sql
@@ -0,0 +1,8 @@
+--MySQLSh
+-- Advanced command_line clent and code editor for MySQL 
+-- Offers scripting capabilities for Python and JavaScript
+-- MySQL Shell Connects to MySQL Server through the x protocol (mysqlx.sock and mysqlx.sock.lock)
+
+Syntax:
+mysqlsh >\connect_mysql <username>@server:port
+
